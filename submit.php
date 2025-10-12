3b2668d15ff8b5d39c9407a3734768dfba7a9e67
Rodedarline7 <pacifirodaline@gmail.com>
2025-10-12 06:57:13 -0400

    Refactor submission response to use PHP echo

--- submit.php
+++ submit.php
@@ -3,19 +3,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = htmlspecialchars($_POST["name"]);
     $email = htmlspecialchars($_POST["email"]);
     $message = htmlspecialchars($_POST["message"]);
-
-    echo "<!DOCTYPE html>
-    <html lang='en'>
-    <head>
-        <meta charset='UTF-8'>
-        <title>Submission Success</title>
-    </head>
-    <body>
-        <h1>Thank you, $name!</h1>
-        <p>Your message has been received successfully.</p>
-        <a href='contact.html'>Back to form</a>
-    </body>
-    </html>";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Submission Success</title>
+</head>
+<body>
+    <h1>Thank you, <?php echo $name; ?>!</h1>
+    <p>Your message has been received successfully.</p>
+    <a href="contact.html">Back to form</a>
+</body>
+</html>
+<?php
 } else {
     header("Location: contact.html");
     exit();
